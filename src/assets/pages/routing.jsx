9f8c296d323b4e99d0e270f2891d2a862df6a860
Ivan Romero Gutierrez <manny.parte.2@gmail.com>
2023-10-04 20:36:55 -0400

    generando otras rutas

--- src/assets/pages/routing.jsx
+++ src/assets/pages/routing.jsx
@@ -4,12 +4,24 @@ const routing = () => {
   return (
     <BrowserRouter>
     <nav>
-        <Link to='/'>Home</Link>
+        <Link to='/'>Auxiliares</Link>
+        <Link to="/Marketplace">Marketplace</Link>
+        <Link to="/Ucount">Tu cuenta</Link>
         <Link to="/About">Acerca</Link>
+        <Link to="/Politics">Politicas de Uso</Link>
+        <Link to="/Error"></Link>
+        <Link to="/Soon"></Link>
     </nav>
     <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/About" element={<About />}/>
+        <Route path="/" element={<Home />}>
+            <Route path="/About" element={<About />}/>
+            <Route path="/Ucount" element={<Ucount />}/>
+            <Route path="/Politics" element={<Politics />}/>
+            <Route path="/Error" element={<Error />}/>
+        </Route>
+        <Route path="/Marketplace" element={<Marketplace />}>
+            <Route path="/Soon" element={<Soon/>}/>
+        </Route>
     </Routes>
 </BrowserRouter>
   )
